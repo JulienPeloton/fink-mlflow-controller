7201b27aba7e9453c9b45d77fd5e6617c44008dd
JulienPeloton <peloton@lal.in2p3.fr>
2025-10-07 16:18:28 +0200

    Initial commit for user creation/deletion

---
+++ fink_mlflow_controller/logger.py
@@ -0,0 +1,41 @@
+import logging
+from logging import Logger
+
+
+def get_fink_logger(name: str = "test", log_level: str = "INFO") -> Logger:
+    """Initialise python logger. Suitable for both driver and executors.
+
+    Parameters
+    ----------
+    name : str
+        Name of the application to be logged. Typically __name__ of a
+        function or module.
+    log_level : str
+        Minimum level of log wanted: DEBUG, INFO, WARNING, ERROR, CRITICAL, OFF
+
+    Returns
+    -------
+    logger : logging.Logger
+        Python Logger
+
+    Examples
+    --------
+    >>> log = get_fink_logger(__name__, "INFO")
+    >>> log.info("Hi!")
+    """
+    # Format of the log message to be printed
+    FORMAT = "%(asctime)-15s "
+    FORMAT += "%(levelname)s "
+    FORMAT += "%(message)s"
+
+    # Date format
+    DATEFORMAT = "%y/%m/%d %H:%M:%S"
+
+    logging.basicConfig(format=FORMAT, datefmt=DATEFORMAT)
+    logger = logging.getLogger(name)
+
+    # Set the minimum log level
+    logger.setLevel(log_level)
+
+    return logger
+
---
+++ fink_mlflow_controller/scripts/fink_mlflow.py
@@ -0,0 +1,62 @@
+import requests
+import os
+import sys
+import argparse
+
+from fink_mlflow_controller.logger import get_fink_logger
+
+def main():
+    """User management in MLFlow Fink"""
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    # Create subparsers for 'create' and 'delete'
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    # Create parser for 'create' command
+    create_parser = subparsers.add_parser('create', help='Create a user')
+    create_parser.add_argument('-username', type=str, help='The user name', required=True)
+    create_parser.add_argument('-password', type=str, help='The password. Must be at least 12 characters', required=True)
+    create_parser.add_argument(
+        "--is_admin",
+        action="store_true",
+        help="If specified, promote the user as admin.",
+    )
+
+    # Create parser for 'delete' command
+    delete_parser = subparsers.add_parser('delete', help='Delete a user')
+    delete_parser.add_argument('-username', type=str, help='The user name', required=True)
+
+    args = parser.parse_args(None)
+
+    logger = get_fink_logger("mlflow-users", "INFO")
+
+    if args.command == "create":
+        # Create user
+        r = requests.post(
+            "https://mlflow-dev.fink-broker.org/api/2.0/mlflow/users/create",
+            json={"username": args.username, "password": args.password},
+            auth=("fink", os.environ["FINK_ADMIN_PWD"])
+        )
+
+        if r.status_code != 200:
+            logger.error("Something went wrong in creating user with username {} and password {}: {}".format(args.username, args.password, r.content))
+            sys.exit(1)
+
+        logger.info("User {} with password {} created".format(args.username, args.password))
+        
+        if args.is_admin:
+            r = requests.patch(
+                "https://mlflow-dev.fink-broker.org/api/2.0/mlflow/users/update-admin",
+                json={"username": args.username, "is_admin": True},
+                auth=("fink", os.environ["FINK_ADMIN_PWD"])
+            )
+
+            if r.status_code != 200:
+                logger.error("Something went wrong in promoting {} as admin: {}".format(args.username, r.content))
+                sys.exit(1)
+
+            logger.info("User {} promoted admin successfuly".format(args.username, args.password))
+
+if __name__ == "__main__":
+    main()
+
